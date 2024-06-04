6ae6df5e45936afa4b1b17c0ea2f42a2000ed89c
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-06-04 15:46:06 -0300

    add de login form

--- app/pages/Login.tsx
+++ app/pages/Login.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Formik, Field, Form } from 'formik';
+
+interface Values {
+  username: string;
+  password: string;
+}
 
 const Login = () => {
   return (
     <div>
-      
+<Formik
+            initialValues={{
+                username: '',
+                password: '',
+            }}
+
+            onSubmit={() => {
+
+            }}
+        >
+            <Form>
+                <Field id="username" name="username" placeholder="Username" />
+                <Field type="password" id="password" name="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </Form>
+        </Formik>
     </div>
   )
 }
--- app/pages/Mapas.tsx
+++ app/pages/Mapas.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Mapas = () => {
   return (
     <div>
-      
+      <h1>Aonde estão os incineradores</h1>
+      //pesquisa pelo CEP
+      // aqui tem uma imagem de mapa 
     </div>
   )
 }
