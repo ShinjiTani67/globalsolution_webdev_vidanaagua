c75050d902df22cb8e99edbb1c9d36c0c50ef739
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-06-05 17:22:14 -0300

    atualizacao de header e footer

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const header = () => {
   return (
     <div className='header'>
-      
+      <div>Home</div>
+      <div>Atualidades</div>
+      <div>Mapas</div>
+      <div>Login</div>
+      <div>Membros</div>
     </div>
   )
 }
