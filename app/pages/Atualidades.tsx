8e9a7651bb55bae3861a43112eb69085e90eb49f
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-06-05 17:17:43 -0300

    atualizacao geral

--- app/pages/Atualidades.tsx
+++ app/pages/Atualidades.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Atualidades = () => {
   return (
     <div>
-      
+      <h1>Atualidades</h1>
+      <p>noticias sobre reciclagem</p>
+      <div>//imagem de coisa</div>      
     </div>
   )
 }
