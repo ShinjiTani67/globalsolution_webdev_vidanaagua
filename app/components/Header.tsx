6a6abf84e7a45a4ee604940d5148c9824879a600
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-06-06 16:41:32 -0300

    update do css

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link'
 const header = () => {
   return (
     <div className='header'>
-      <nav>
+      <div className='headerRight'><Link href='/'></Link></div>
+      <nav className='headerLeft'>
         <ul>
         <li><Link href="/atualidades">Atualidades</Link></li>
         <li><Link href="/mapas">mapas</Link></li>
