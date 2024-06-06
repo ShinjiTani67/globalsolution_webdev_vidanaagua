e8b191eb751e11ef39cc19c19a97b17de0c0daea
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-06-06 15:40:30 -0300

    update em membros

--- app/pages/Membros/pages.tsx
+++ app/pages/Membros/pages.tsx
@@ -3,7 +3,13 @@ import React from 'react'
 const  membros = () => {
   return (
     <div>
-      
+        <nav>
+            <ul>
+                <li>Fernando Tanigushi RM</li>
+                <li>Guilherme Gambarão RM</li>
+                <li>João Vitor Valaitis RM</li>
+            </ul>
+        </nav> 
     </div>
   )
 }
