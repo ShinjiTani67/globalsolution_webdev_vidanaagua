49519f9f2a3885d4699621089eb83a3683034ef7
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-06-06 22:13:29 -0300

    update login

--- app/pages/Login/page.tsx
+++ app/pages/Login/page.tsx
@@ -1,32 +1,40 @@
 import React from 'react'
-import { Formik, Field, Form } from 'formik';
+import Link from 'next/link'
+import Header from '@/app/components/Header';
+import Footer from '@/app/components/Footer';
 
-interface Values {
-  username: string;
-  password: string;
-}
+const Login = () => {
 
-const login = () => {
-  return (
-    <div>
-<Formik
-            initialValues={{
-                username: '',
-                password: '',
-            }}
-
-            onSubmit={() => {
-
-            }}
-        >
-            <Form>
-                <Field id="username" name="username" placeholder="Username" />
-                <Field type="password" id="password" name="password" placeholder="Password" />
-                <button type="submit">Login</button>
-            </Form>
-        </Formik>
+  return(
+    <>
+    <Header></Header>
+    <div className='login'>
+      <div className="text"></div>
+      <div>
+          <h1>Login</h1>
+          <div>
+            <input type="email"/>
+            <label htmlFor="">E-mail</label>
+          </div>
+          <div>
+            <input type="psw"/>
+            <label htmlFor="">Senha</label>
+          </div>
+          <div>
+            <div>
+              <input type="checkbox" name="" id=""/>
+              <label htmlFor="Remember Me">Lembrar do login</label>
+            </div>
+            <button type="submit">Login</button>
+            <div>
+              <span>Novo por aqui? <Link href='/cadastro'>Crie uma conta</Link></span>
+            </div>
+          </div>
+      </div>
     </div>
+    <Footer></Footer>
+    </>
   )
 }
 
-export default login
+export default Login
