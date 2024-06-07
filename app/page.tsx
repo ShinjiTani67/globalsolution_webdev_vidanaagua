f9cd1d7c7a1a6cd96abee7c970047e2b8e419bb2
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-06-07 08:22:15 -0300

    update geral

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 import React from 'react'
 import Header from './components/Header';
-import ocean_pic2 from './assets/ocean_pic2.jpg'
+import Link from 'next/link'
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
     <>
     <Header></Header>
-    <img className='imgHome' src={ocean_pic2}></img>
+    <img className='imgHome' src="/ocean_pic2.jpg"></img>
     <section className='apresentacao'>
-    <div>conheça um pouco do projeto</div>
-    <p className="apresentacaoHome"></p>
+      <div>conheça um pouco do projeto</div>
+      <p className="apresentacaoHome"></p>
+    </section>
     <button><Link href='/mapa'>Mapa</Link></button>
     <button><Link href='/atualidades'>Atualidades</Link></button>
     <button><Link href='/membros'></Link></button>
