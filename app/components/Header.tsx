2048c80ab246a741e2e609990047b3260610b95c
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-06-07 08:58:34 -0300

    update geral

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link'
 const Header = () => {
   return (
     <div className='header'>
-      <div className='headerRight'><Link href='/'></Link></div>
+      <div className='headerRight'><Link href='/'><img src='/app/assets/cycling_logo.png'></img></Link></div>
       <nav className='headerLeft'>
         <ul>
         <li><Link href="/atualidades">Atualidades</Link></li>
-        <li><Link href="/mapas">mapas</Link></li>
+        <li><Link href="/mapas">Mapas</Link></li>
         <li></li>
         <li><Link href="/membros">Membros</Link></li>
         </ul>
