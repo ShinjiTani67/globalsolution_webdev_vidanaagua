d1433e81cdf0622f0309e9fcf0316a120ac6ffb9
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-06-07 08:12:05 -0300

    update cadastro

--- app/pages/Cadastro/page.tsx
+++ app/pages/Cadastro/page.tsx
@@ -3,7 +3,11 @@ import { useState } from 'react';
 export default function Cadastro() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
-  
+  const [cep, setCep] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [senha, setSenha] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -30,6 +34,34 @@ export default function Cadastro() {
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
+        <label htmlFor="cep">CEP:</label>
+          <input
+            type="cep"
+            id="cep"
+            value={cep}
+            onChange={(e) => setCep(e.target.value)}
+          />
+          <label htmlFor="endereco">Endereço:</label>
+          <input
+            type="endereco"
+            id="endereco"
+            value={endereco}
+            onChange={(e) => setEndereco(e.target.value)}
+          />
+          <label htmlFor="telefone">Telefone:</label>
+          <input
+            type="telefone"
+            id="telefone"
+            value={telefone}
+            onChange={(e) => setTelefone(e.target.value)}
+          />
+                    <label htmlFor="senha">Senha:</label>
+          <input
+            type="senha"
+            id="senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
         <button type="submit">Cadastrar</button>
       </form>
     </div>
