dfd5687f22091d64b7c8ad8755d3154421658a72
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-06-07 18:56:43 -0300

    update footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,9 @@ const Footer = () => {
   return (
     <div className='footer'>
       <p>Copyright</p>
-      <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+      <p>Fernando Tanigushi RM553587
+          Guilherme Gambarão RM554258
+          João Vitor Valaitis RM55397</p>
     </div>
   )
 }
