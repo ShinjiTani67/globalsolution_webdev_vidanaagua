2c46531e198aac46f0cd04a10d45a7bad30e3700
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-06-07 01:13:30 -0300

    update do geral

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import React from 'react'
 import Header from './components/Header';
-
+import ocean_pic2 from './assets/ocean_pic2.jpg'
 export default function Home() {
   return (
     <>
     <Header></Header>
-    <img className='imgHome'></img>
+    <img className='imgHome' src={ocean_pic2}></img>
     <section className='apresentacao'>
     <div>conheça um pouco do projeto</div>
     </section>
